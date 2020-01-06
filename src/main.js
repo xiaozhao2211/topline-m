3e2a0307e1efe5de2ff996993d475fdd22a2d536
zhaodongxue <1510386905@qq.com>
2020-01-06 20:40:26 +0800

    处理后端返回数据超出 JS 安全整数范围问题

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './utils/register-vant'
+import 'amfe-flexible'
 
+import './styles/index.less'
 Vue.config.productionTip = false
 
 new Vue({
