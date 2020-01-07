d29632f0aa362b69482ac4e4291e3183fda67b60
zhaodongxue <1510386905@qq.com>
2020-01-07 15:14:58 +0800

    用户登录注册-请求登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,25 +4,40 @@
       <van-nav-bar title="登录"></van-nav-bar>
       <!-- 表单 -->
       <van-cell-group>
-        <van-field label="用户名" placeholder="请输入用户名" clearable ></van-field>
-        <van-field label="验证码" placeholder="请输入验证码" clearable >
+        <van-field label="用户名" placeholder="请输入用户名" clearable v-model="user.mobile"></van-field>
+        <van-field label="验证码" placeholder="请输入验证码" clearable v-model="user.code">
             <van-button slot="button" size="small" type="primary">发送验证码</van-button>
         </van-field>
       </van-cell-group>
       <div class="btn-wrap">
-        <van-button type="info" >登录</van-button>
+        <van-button type="info" @click="userLogin">登录</van-button>
       </div>
   </div>
 </template>
 
 <script>
+import { login } from '@/api/user'
 export default {
   name: 'LoginPage',
   data () {
     return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    async userLogin () {
+      const user = this.user
+      try {
+        let result = await login(user)
+        console.log(result)
+      } catch (error) {
+        console.log('登录失败', error)
+      }
     }
   }
-
 }
 </script>
 
