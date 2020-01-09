9709320b7cb4a7ed9ac954de4d817096693c0a18
zhaodongxue <1510386905@qq.com>
2020-01-09 21:29:27 +0800

    Token处理-发送token

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,8 +2,8 @@
  * 封装 axios 请求模块
  */
 import axios from 'axios'
-
 import jsonBig from 'json-bigint'
+import store from '@/store'
 // axios.create 方法：复制一个 axios
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
@@ -15,4 +15,26 @@ request.defaults.transformResponse = [function (data) {
     return {}
   }
 }]
+// 1、请求拦截器
+request.interceptors.request.use(function (config) {
+  // config 请求配置对象，我们可以通过修改 config 来实现统一请求数据处理
+  const { user } = store.state
+  // 在发送请求之前做些什么，例如加入token
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+
+// 2、响应拦截器
+request.interceptors.response.use(function (response) {
+  // 在接收响应做些什么，例如跳转到登录页
+  return response
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error)
+})
 export default request
