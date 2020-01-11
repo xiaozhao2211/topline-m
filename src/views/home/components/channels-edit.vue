35e70b641e23cd6f349c61347b79765f656ae2f4
zhaodongxue <1510386905@qq.com>
2020-01-11 20:56:35 +0800

    首页-频道管理-组件布局

--- src/views/home/components/channels-edit.vue
+++ src/views/home/components/channels-edit.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="channels-edit">频道编辑</div>
+  <div class="channels-edit">
+      <!-- 我的频道-->
+      <van-cell title="我的频道" :border="false">
+        <van-button size="mini" round type="danger" plain>编辑</van-button>
+      </van-cell>
+      <van-grid :gutter="10">
+        <van-grid-item
+         v-for="value in 8"
+         :key="value"
+         text="文字"
+        />
+      </van-grid>
+
+      <!-- 推荐频道 -->
+      <van-cell title="推荐频道" :border="false"></van-cell>
+      <van-grid :gutter="10">
+        <van-grid-item
+         v-for="value in 8"
+         :key="value"
+         text="文字"
+        />
+      </van-grid>
+  </div>
 </template>
 
 <script>
@@ -8,6 +30,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+ .channels-edit {
+ padding: 40px 0;
+}
 </style>
