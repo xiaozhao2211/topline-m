de3e7b2af90bf23cc5881d1d4e0858d7244c7fff
zhaodongxue <1510386905@qq.com>
2020-01-11 22:05:58 +0800

    首页-频道管理-展示推荐频道

--- src/views/home/components/channels-edit.vue
+++ src/views/home/components/channels-edit.vue
@@ -16,20 +16,51 @@
       <van-cell title="推荐频道" :border="false"></van-cell>
       <van-grid :gutter="10">
         <van-grid-item
-         v-for="value in 8"
-         :key="value"
-         text="文字"
+         v-for="channel in remainingChannels"
+         :key="channel.id"
+         :text="channel.name"
         />
       </van-grid>
   </div>
 </template>
 
 <script>
+import { getAllChannels } from '@/api/channels'
 export default {
   props: {
     channelList: {
       type: Array,
       required: true
+    }
+  },
+  data () {
+    return {
+      allChannels: []// 接收所有频道
+    }
+  },
+  methods: {
+    // 获取所有频道
+    async loadAllChannels () {
+      const { data } = await getAllChannels()
+      this.allChannels = data.data.channels
+    //   console.log(this.allChannels)
+    }
+  },
+  created () {
+    this.loadAllChannels()
+  },
+  computed: {
+    remainingChannels () {
+      const { channelList, allChannels } = this
+      const channels = []
+      // 遍历所有频道
+      allChannels.forEach(item => {
+        // 如果我的频道中不包含当前被遍历的频道，则要
+        if (!channelList.find(c => c.id === item.id)) {
+          channels.push(item)
+        }
+      })
+      return channels
     }
   }
 }
