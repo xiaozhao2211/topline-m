94d50f9005e6a70c40a5e79d907ce7fff632af7b
zhaodongxue <1510386905@qq.com>
2020-01-15 11:37:10 +0800

    文章详情-JWT获取用户id、处理关注按钮

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { getItem, setItem } from '@/utils/storage'
+import decodeJwt from 'jwt-decode'
 
 Vue.use(Vuex)
 
@@ -10,6 +11,10 @@ export default new Vuex.Store({
   },
   mutations: {
     setUser (state, data) {
+      // 解析 JWT 中的数据（需要使用用户ID）
+      if (data && data.token) {
+        data.id = decodeJwt(data.token).user_id
+      }
       state.user = data
       // 将数据存储到本地存储 持久化
       setItem('user', state.user)
