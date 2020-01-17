bc3a52afc4f1a0af9257a21ee9478ee1e530818d
zhaodongxue <1510386905@qq.com>
2020-01-17 11:24:28 +0800

    编辑用户资料-页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,13 +1,40 @@
 <template>
-  <div class="user-pofile">编辑资料</div>
+  <div class="user-profile">
+    <van-nav-bar
+        title="个人信息"
+        left-arrow
+        @click="$router.back()"
+    />
+    <van-cell-group>
+        <van-cell title="头像" is-link>
+            <van-image
+             round
+             width="30"
+             height="30"
+             src="https://img.yzcdn.cn/vant/cat.jpeg"
+            />
+        </van-cell>
+        <van-cell title="昵称" value="内容" is-link/>
+        <van-cell title="性别" value="内容" is-link/>
+        <van-cell title="生日" value="内容" is-link/>
+    </van-cell-group>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'UserProfile',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
 }
 </script>
 
-<style>
-
-</style>
+<style scoped></style>
